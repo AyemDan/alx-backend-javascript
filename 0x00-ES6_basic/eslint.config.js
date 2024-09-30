5dc910ded58a9ea4bf06f9af7c2158fced71038d
ayemdanm <danielMMomodu@gmail.com>
2024-09-30 21:15:36 +0100

    modified files

--- 0x00-ES6_basic/eslint.config.js
+++ 0x00-ES6_basic/eslint.config.js
@@ -1,35 +1,32 @@
-module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
+  module.exports = {
     extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+        'airbnb-base',
+        'plugin:jest/all',
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+    languageOptions: {
+        globals: {
+            Atomics: 'readonly',
+            SharedArrayBuffer: 'readonly',
+        },
     },
     parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
+        ecmaVersion: 2020,
+        sourceType: 'module',
     },
     plugins: ['jest'],
     rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
     },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        },
+    ],
+};
