bec000dd2dea612c118294e6e56c517c731b2184
ayemdanm <danielMMomodu@gmail.com>
2024-10-04 00:26:31 +0100

    update

--- 0x00-ES6_basic/eslint.config.mjs
+++ 0x00-ES6_basic/eslint.config.mjs
@@ -1,31 +1,30 @@
-// eslint.config.js
-
-import globals from "globals";
-import js from "@eslint/js";
-import jestPlugin from "eslint-plugin-jest";
-import importPlugin from "eslint-plugin-import"; // Import the eslint-plugin-import
+/* eslint-disable import/no-extraneous-dependencies */
+import globals from 'globals';
+import js from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import'; // Import the eslint-plugin-import
 
 // Import specific Airbnb rule files
-import bestPracticesRules from "eslint-config-airbnb-base/rules/best-practices";
-import errorsRules from "eslint-config-airbnb-base/rules/errors";
-import nodeRules from "eslint-config-airbnb-base/rules/node";
-import styleRules from "eslint-config-airbnb-base/rules/style";
-import variablesRules from "eslint-config-airbnb-base/rules/variables";
-import es6Rules from "eslint-config-airbnb-base/rules/es6";
-import importsRules from "eslint-config-airbnb-base/rules/imports";
-import strictRules from "eslint-config-airbnb-base/rules/strict";
+import bestPracticesRules from 'eslint-config-airbnb-base/rules/best-practices';
+import errorsRules from 'eslint-config-airbnb-base/rules/errors';
+import nodeRules from 'eslint-config-airbnb-base/rules/node';
+import styleRules from 'eslint-config-airbnb-base/rules/style';
+import variablesRules from 'eslint-config-airbnb-base/rules/variables';
+import es6Rules from 'eslint-config-airbnb-base/rules/es6';
+import importsRules from 'eslint-config-airbnb-base/rules/imports';
+import strictRules from 'eslint-config-airbnb-base/rules/strict';
 
 export default [
   js.configs.recommended, // Use recommended base rules from ESLint
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...globals.node,
-        myCustomGlobal: "readonly",
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
+        myCustomGlobal: 'readonly',
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
       },
     },
     plugins: {
@@ -42,21 +41,21 @@ export default [
       ...es6Rules.rules,
       ...importsRules.rules,
       ...strictRules.rules,
-  //     'import/extensions': [
-  //   'error',
-  //   'always', // or 'never' to disallow extensions
-  //   {
-  //     js: 'always', // Set to 'always' to allow the use of .js extensions
-  //   },
-  // ],
-      
+      //     'import/extensions': [
+      //   'error',
+      //   'always', // or 'never' to disallow extensions
+      //   {
+      //     js: 'always', // Set to 'always' to allow the use of .js extensions
+      //   },
+      // ],
+
       // Your custom rules
-      "no-console": "off",
-      "no-shadow": "off",
-      "no-restricted-syntax": [
-        "error",
-        "LabeledStatement",
-        "WithStatement",
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
       ],
     },
   },
