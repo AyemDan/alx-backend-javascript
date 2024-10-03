bf55359899ea7c363b3833b75625024d90b8d5e0
ayemdanm <danielMMomodu@gmail.com>
2024-10-04 00:52:29 +0100

    update2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use 'let' instead of 'var'
-  let task2 = true; // Use 'let' instead of 'var'
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = false; // Update the value instead of redeclaring
-    task2 = true; // Update the value instead of redeclaring
+    // No need to redeclare task and task2 here since the condition should not affect the result
   }
 
   return [task, task2];
