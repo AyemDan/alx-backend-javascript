542f13d1e84d3d053d1d79f3b59a6d320b00029f
ayemdanm <danielMMomodu@gmail.com>
2024-10-05 18:25:07 +0100

    Done tasks - 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => [
-      {
-        status: results.status,
-        value: results.value,
-      },
-    ]);
+    .then((results) => {
+      const processedResults = [];
+      results.forEach((result) => {
+        processedResults.push({
+          status: result.status,
+          value: result.value 
+      });
+      return processedResults;
+    });
 }
