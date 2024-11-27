fa46d54eb81a2c1d1c1b7b561bf30adab26a7b8c
AyemDanm <danielMMomodu@gmail.com>
2024-11-27 15:34:49 +0100

    Read file2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' }).trim();
-    const rows = data.split('\n').filter((row) => row.trim() !== '');
-
-    if (rows.length < 2) { // Ensure there's at least a header and one data row
+    if (!path || !fs.existsSync(path)) {
       throw new Error('Cannot load the database');
     }
+    const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' }).trim();
+    const rows = data.split('\n').filter((row) => row.trim() !== '');
+    if (rows.length === 0) {
+      throw new Error('File is invalid');
+    }
 
-    const fields = rows[0].split(',');
-    const students = rows.slice(1).map((row) => row.split(','));
+    const fields = rows[0].split(',').map((header) => header.trim());
 
-    const studentObj = students.map((student) => fields.reduce(
-      (newObj, key, index) => ({ ...newObj, [key]: student[index] }),
-      {},
-    ));
+    const students = rows.slice(1).map((row) => row.split(',').map((cell) => cell.trim()));
+
+    const studentObj = students.map((student) => fields.reduce((newObj, key, index) => (
+      { ...newObj, [key]: student[index] }), {}));
 
     const numOfStudents = studentObj.length;
     const studentsByField = studentObj.reduce((acc, student) => {
@@ -32,7 +33,7 @@ function countStudents(path) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     });
   } catch (err) {
-    console.error('Cannot load the database');
+    console.error(err);
   }
 }
 
