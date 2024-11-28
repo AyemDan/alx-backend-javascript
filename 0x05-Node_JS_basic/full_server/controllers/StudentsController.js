a840b4215058b9220d3ecf473aff156067132346
AyemDanm <danielMMomodu@gmail.com>
2024-11-28 12:04:12 +0100

    Format

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,66 +1,64 @@
 const readDatabase = require('../utils'); // Adjust the path as needed
 
 class StudentsController {
-    static async getAllStudents(request, response) {
-        try {
-            const databasePath = process.argv[2] || request.query.database || 'database.csv'
-            const data = await readDatabase(databasePath);
-            
-            const fields = {};
+  static async getAllStudents(request, response) {
+    try {
+      const databasePath = process.argv[2] || request.query.database || 'database.csv';
+      const data = await readDatabase(databasePath);
 
-            data.forEach(student => {
-                const { firstname, field } = student;
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-            });
+      const fields = {};
 
-            let responseMessage = "This is the list of our students\n";
-            
-            for (const field in fields) {
-                const studentList = fields[field];
-                responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
-            }
-            
-
-            // Send the response with status 200
-            response.status(200).send(responseMessage);
-
-        } catch (error) {
-            console.log(error);
-            response.status(500).send('Cannot load the database');
+      data.forEach((student) => {
+        const { firstname, field } = student;
+        if (!fields[field]) {
+          fields[field] = [];
         }
-    }
-    
-    static async getAllStudentsByMajor(request, response) {
-        try {
-            const major = request.params.major;
+        fields[field].push(firstname);
+      });
 
-            if (major !== 'CS' && major !== 'SWE') {
-                return response.status(500).send('Major parameter must be CS or SWE');
-            }
-            const databasePath = process.argv[2] || request.query.database || 'database.csv'
-            const data = await readDatabase(databasePath);
+      let responseMessage = 'This is the list of our students\n';
 
-            const studentsInMajor = data.filter(student => student.field === major);
-
-            const firstNames = studentsInMajor.map(student => student.firstname);
-
-            if (firstNames.length === 0) {
-                return response.status(200).send(`No students found in ${major}`);
-            }
-
-            const responseMessage = `List: ${firstNames.join(', ')}`;
-
-            response.status(200).send(responseMessage);
-
-        } catch (error) {
-            // If the database cannot be loaded, return a 500 status with an error message
-            console.log(error);
-            response.status(500).send('Cannot load the database');
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          const studentList = fields[field];
+          responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
         }
+      }
+
+      response.status(200).send(responseMessage);
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
     }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    try {
+      const { major } = request.params;
+
+      if (major !== 'CS' && major !== 'SWE') {
+        return response.status(500).send('Major parameter must be CS or SWE');
+      }
+      const databasePath = process.argv[2] || request.query.database || 'database.csv';
+      const data = await readDatabase(databasePath);
+
+      const studentsInMajor = data.filter((student) => student.field === major);
+
+      const firstNames = studentsInMajor.map((student) => student.firstname);
+
+      if (firstNames.length === 0) {
+        return response.status(200).send(`No students found in ${major}`);
+      }
+
+      const responseMessage = `List: ${firstNames.join(', ')}`;
+
+      response.status(200).send(responseMessage);
+    } catch (error) {
+      console.log(error);
+      response.status(500).send('Cannot load the database');
+    }
+
+    return null; // Explicit return to avoid linting error
+  }
 }
 
 module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -8,7 +8,7 @@ app.use('/', routes);
 
 // Start the server on port 1245
 app.listen(1245, () => {
-    console.log('Server is running on http://localhost:1245');
+  console.log('Server is running on http://localhost:1245');
 });
 
 module.exports = app;
