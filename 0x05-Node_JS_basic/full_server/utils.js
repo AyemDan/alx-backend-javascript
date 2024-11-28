91169843db6718b2bc1a1500d11f834290a041c2
AyemDanm <danielMMomodu@gmail.com>
2024-11-28 12:39:12 +0100

    change7

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,27 +1,30 @@
 const fs = require('fs').promises;
 
 async function readDatabase(filePath) {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
+        
+        // Split the data into lines
+        const lines = data.trim().split('\n');
+        
+        // Parse the header and rows
+        const [header, ...rows] = lines;
+        const headers = header.split(',').map(h => h.trim());
+        const row = rows.map(row => row.split(',').map(r => r.trim()));
 
-    // Split the data into lines
-    const lines = data.trim().split('\n');
-
-    // Parse the header and rows
-    const [header, ...rows] = lines;
-    const headers = header.split(',').map((h) => h.trim());
-    const row = rows.map((row) => row.split(',').map((r) => r.trim()));
-
-    const dictionary = row.map((r) => r.reduce((acc, value, index) => {
-        const newObj = { ...acc }; // Create a new object to avoid direct mutation
-        newObj[headers[index]] = value;
-        return newObj;
-    }, {}));
-
-    return dictionary;
-  } catch (err) {
-    console.log(err);
-  }
+        
+        const dictionary = row.map(r => {
+            return r.reduce((obj, value, index) => {
+              obj[headers[index]] = value; 
+              return obj;
+            }, {});
+          });
+          
+        return dictionary
+    } catch (err) {
+        // Reject the promise with the error
+        throw err;
+    }
 }
 
 module.exports = readDatabase;
