58099f3581ef612e8458e7eb2548d8fd3ab9646c
AyemDanm <danielMMomodu@gmail.com>
2024-11-28 12:41:04 +0100

    change8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,66 +1,63 @@
 const readDatabase = require('../utils'); // Adjust the path as needed
 
 class StudentsController {
-    static async getAllStudents(request, response) {
-        try {
-            const databasePath = process.argv[2] || request.query.database || 'database.csv'
-            const data = await readDatabase(databasePath);
-            
-            const fields = {};
+  static async getAllStudents(request, response) {
+    try {
+      const databasePath = process.argv[2] || request.query.database || 'database.csv';
+      const data = await readDatabase(databasePath);
 
-            data.forEach(student => {
-                const { firstname, field } = student;
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-            });
+      const fields = {};
 
-            let responseMessage = "This is the list of our students\n";
-            
-            for (const field in fields) {
-                const studentList = fields[field];
-                responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
-            }
-            
-
-            // Send the response with status 200
-            response.status(200).send(responseMessage);
-
-        } catch (error) {
-            console.log(error);
-            response.status(500).send('Cannot load the database');
+      data.forEach((student) => {
+        const { firstname, field } = student;
+        if (!fields[field]) {
+          fields[field] = [];
         }
+        fields[field].push(firstname);
+      });
+
+      let responseMessage = 'This is the list of our students\n';
+
+      for (const field in fields) {
+        const studentList = fields[field];
+        responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
+      }
+
+      // Send the response with status 200
+      response.status(200).send(responseMessage);
+    } catch (error) {
+      console.log(error);
+      response.status(500).send('Cannot load the database');
     }
-    
-    static async getAllStudentsByMajor(request, response) {
-        try {
-            const major = request.params.major;
+  }
 
-            if (major !== 'CS' && major !== 'SWE') {
-                return response.status(500).send('Major parameter must be CS or SWE');
-            }
-            const databasePath = process.argv[2] || request.query.database || 'database.csv'
-            const data = await readDatabase(databasePath);
+  static async getAllStudentsByMajor(request, response) {
+    try {
+      const { major } = request.params;
 
-            const studentsInMajor = data.filter(student => student.field === major);
+      if (major !== 'CS' && major !== 'SWE') {
+        return response.status(500).send('Major parameter must be CS or SWE');
+      }
+      const databasePath = process.argv[2] || request.query.database || 'database.csv';
+      const data = await readDatabase(databasePath);
 
-            const firstNames = studentsInMajor.map(student => student.firstname);
+      const studentsInMajor = data.filter((student) => student.field === major);
 
-            if (firstNames.length === 0) {
-                return response.status(200).send(`No students found in ${major}`);
-            }
+      const firstNames = studentsInMajor.map((student) => student.firstname);
 
-            const responseMessage = `List: ${firstNames.join(', ')}`;
+      if (firstNames.length === 0) {
+        return response.status(200).send(`No students found in ${major}`);
+      }
 
-            response.status(200).send(responseMessage);
+      const responseMessage = `List: ${firstNames.join(', ')}`;
 
-        } catch (error) {
-            // If the database cannot be loaded, return a 500 status with an error message
-            console.log(error);
-            response.status(500).send('Cannot load the database');
-        }
+      response.status(200).send(responseMessage);
+    } catch (error) {
+      // If the database cannot be loaded, return a 500 status with an error message
+      console.log(error);
+      response.status(500).send('Cannot load the database');
     }
+  }
 }
 
 module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,30 +1,27 @@
 const fs = require('fs').promises;
 
 async function readDatabase(filePath) {
-    try {
-        const data = await fs.readFile(filePath, 'utf8');
-        
-        // Split the data into lines
-        const lines = data.trim().split('\n');
-        
-        // Parse the header and rows
-        const [header, ...rows] = lines;
-        const headers = header.split(',').map(h => h.trim());
-        const row = rows.map(row => row.split(',').map(r => r.trim()));
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
 
-        
-        const dictionary = row.map(r => {
-            return r.reduce((obj, value, index) => {
-              obj[headers[index]] = value; 
-              return obj;
-            }, {});
-          });
-          
-        return dictionary
-    } catch (err) {
-        // Reject the promise with the error
-        throw err;
-    }
+    // Split the data into lines
+    const lines = data.trim().split('\n');
+
+    // Parse the header and rows
+    const [header, ...rows] = lines;
+    const headers = header.split(',').map((h) => h.trim());
+    const row = rows.map((row) => row.split(',').map((r) => r.trim()));
+
+    const dictionary = row.map((r) => r.reduce((obj, value, index) => {
+      obj[headers[index]] = value;
+      return obj;
+    }, {}));
+
+    return dictionary;
+  } catch (err) {
+    // Reject the promise with the error
+    throw err;
+  }
 }
 
 module.exports = readDatabase;
