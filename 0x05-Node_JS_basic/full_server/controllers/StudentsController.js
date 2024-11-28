abeb4df7d3c92d6845f6c29032989450fc1fbe0f
AyemDanm <danielMMomodu@gmail.com>
2024-11-28 12:29:34 +0100

    change 5

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,62 +1,66 @@
 const readDatabase = require('../utils'); // Adjust the path as needed
 
 class StudentsController {
-  static async getAllStudents(request, response) {
-    try {
-      const databasePath = process.argv[2] || request.query.database || 'database.csv';
-      const data = await readDatabase(databasePath);
+    static async getAllStudents(request, response) {
+        try {
+            const databasePath = process.argv[2] || request.query.database || 'database.csv'
+            const data = await readDatabase(databasePath);
+            
+            const fields = {};
 
-      const fields = {};
+            data.forEach(student => {
+                const { firstname, field } = student;
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+                fields[field].push(firstname);
+            });
 
-      data.forEach((student) => {
-        const { firstname, field } = student;
-        if (!fields[field]) {
-          fields[field] = [];
+            let responseMessage = "This is the list of our students\n";
+            
+            for (const field in fields) {
+                const studentList = fields[field];
+                responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
+            }
+            
+
+            // Send the response with status 200
+            response.status(200).send(responseMessage);
+
+        } catch (error) {
+            console.log(error);
+            response.status(500).send('Cannot load the database');
         }
-        fields[field].push(firstname);
-      });
-
-      let responseMessage = 'This is the list of our students\n';
-
-      for (const field in fields) {
-        const studentList = fields[field];
-        responseMessage += `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`;
-      }
-
-      response.status(200).send(responseMessage);
-    } catch (error) {
-      console.log(error);
-      response.status(500).send('Cannot load the database');
     }
-  }
+    
+    static async getAllStudentsByMajor(request, response) {
+        try {
+            const major = request.params.major;
 
-  static async getAllStudentsByMajor(request, response) {
-    try {
-      const { major } = request.params;
+            if (major !== 'CS' && major !== 'SWE') {
+                return response.status(500).send('Major parameter must be CS or SWE');
+            }
+            const databasePath = process.argv[2] || request.query.database || 'database.csv'
+            const data = await readDatabase(databasePath);
 
-      if (major !== 'CS' && major !== 'SWE') {
-        return response.status(500).send('Major parameter must be CS or SWE');
-      }
-      const databasePath = process.argv[2] || request.query.database || 'database.csv';
-      const data = await readDatabase(databasePath);
+            const studentsInMajor = data.filter(student => student.field === major);
 
-      const studentsInMajor = data.filter((student) => student.field === major);
+            const firstNames = studentsInMajor.map(student => student.firstname);
 
-      const firstNames = studentsInMajor.map((student) => student.firstname);
+            if (firstNames.length === 0) {
+                return response.status(200).send(`No students found in ${major}`);
+            }
 
-      if (firstNames.length === 0) {
-        return response.status(200).send(`No students found in ${major}`);
-      }
+            const responseMessage = `List: ${firstNames.join(', ')}`;
 
-      const responseMessage = `List: ${firstNames.join(', ')}`;
+            response.status(200).send(responseMessage);
 
-      response.status(200).send(responseMessage);
-    } catch (error) {
-      // If the database cannot be loaded, return a 500 status with an error message
-      console.log(error);
-      response.status(500).send('Cannot load the database');
+        } catch (error) {
+            // If the database cannot be loaded, return a 500 status with an error message
+            console.log(error);
+            response.status(500).send('Cannot load the database');
+        }
     }
-  }
 }
 
 module.exports = StudentsController;
